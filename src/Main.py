cb9383c7f8eb3b5eb3b9fbeb9726ca046410bb57
IanRiceDev <the3bears@centurylink.net>
2017-02-28 14:17:56 -0500

    update

---
+++ src/Main.py
@@ -0,0 +1,75 @@
+while True:
+    from hexCodes import *
+    print("1 = Encode")
+    print("2 = Decode")
+    print("3 = Exit")
+    userSelect = input("Make a selection: ")
+
+    if userSelect == "1":
+        rawUserInput = input("enter text to encode: ")
+        userInput = rawUserInput
+
+        def makeUpperCase():
+            global userInput
+            userInput = userInput.upper()
+
+
+
+        def splitInput():
+            global userInput
+            userInput = list(userInput)
+
+
+        makeUpperCase()
+        splitInput()
+
+        i = 0
+
+        useroutput = userInput
+        userOutputLan = len(useroutput)
+
+        while True:
+
+
+            i = i + 1
+            if i > userOutputLan:
+                break
+            print(textTohex[useroutput[i - 1]] + " ", end='')
+        print(" ")
+        print("-----------------------------")
+
+    if userSelect == "2":
+        rawUserInput = input("enter text to decode: ")
+        userInput = rawUserInput
+
+        def stripSpaces():
+            global userInput
+            userInput = userInput.replace(" ","")
+
+
+
+        def splitInput():
+            global userInput
+
+            n = 2
+            userInput = [userInput[i:i + n] for i in range(0, len(userInput), n)]
+
+        stripSpaces()
+
+        splitInput()
+
+        i = 0
+
+        useroutput = userInput
+        userOutputLan = len(useroutput)
+
+        while True:
+
+            i = i + 1
+            if i > userOutputLan:
+                break
+            print(hexTotext[userInput[i - 1]], end='')
+        print(" ")
+        print("-----------------------------")
+    if userSelect == "3":
+        break
